26edb99ac602c3d4c0163d375766a3a0bff48048
Alex Balabon <balabon7@gmail.com>
2020-05-25 20:20:11 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by mac on 25.05.2020.
+//  Copyright © 2020 Aleksandr Balabon. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var TypeLabel: UILabel!
+}
