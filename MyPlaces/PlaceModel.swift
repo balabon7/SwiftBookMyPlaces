6be5e7834613e6da1f8ac01ffd124c06de674fc9
Alex Balabon <balabon7@gmail.com>
2020-05-25 20:42:29 +0300

    Work with Model

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,32 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by mac on 25.05.2020.
+//  Copyright © 2020 Aleksandr Balabon. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+   static let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+   static func getPlaces() -> [Place] {
+        var places = [Place]()
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: place))
+        }
+        return places
+    }
+}
