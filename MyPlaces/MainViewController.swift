c4f10ff87b86ecd225c785953bfa71579bd3dcde
Alex Balabon <balabon7@gmail.com>
2020-05-25 19:09:07 +0300

    Configure the cell

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class MainViewController: UIViewController, UITableViewDataSource, UITabBarDelegate {
  
+    let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,12 +25,13 @@ class MainViewController: UIViewController, UITableViewDataSource, UITabBarDeleg
     
     //MARK: - UItableView Protocols
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         return 10
+        return restaurantNames.count
      }
      
      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Cell"
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.detailTextLabel?.text = "detail"
         return cell
      }
 
