0755d242a7705a07f5c8e9401e12bf3e8ac58008
Alex Balabon <balabon7@gmail.com>
2020-05-26 15:09:45 +0300

    Image Picker Controller

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class NewPlaceViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         navigationItem.title = "New Place"
         
         //Hide unnecessary table lines
@@ -23,6 +23,20 @@ class NewPlaceViewController: UITableViewController {
     // MARK: - Table View Delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
             
         } else {
             view.endEditing(true)
@@ -30,9 +44,8 @@ class NewPlaceViewController: UITableViewController {
     }
 }
 
+// MARK: - Table Field Delegate
 extension NewPlaceViewController: UITextFieldDelegate {
-    
-    // MARK: - Table Field Delegate
     
     //Hide the keyboard by pressing Done
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -41,4 +54,19 @@ extension NewPlaceViewController: UITextFieldDelegate {
         return true
     }
     
+}
+
+// MARK: - Table Field Delegate
+extension NewPlaceViewController {
+
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(source){
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true // allows you to edit
+            imagePicker.sourceType = source // specify the value of source
+            
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
 }
