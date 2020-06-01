87db3b16be91e785b8beb9dca35f7dae32dd2ef2
Alex Balabon <balabon7@gmail.com>
2020-06-02 00:29:04 +0300

    Setup Placemark

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -7,18 +7,53 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    var place : Place!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupPlacemark()
     }
     
     
     @IBAction func closeVC(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
+    
+    private func setupPlacemark() {
+        
+        guard let location = place.location else { return }
+        
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(location) { (placemarks, error) in
+            if let error = error {
+                print("Error:", error)
+                return
+            }
+            
+            guard let placemarks = placemarks else { return }
+            
+            let placemark = placemarks.first
+            
+            let annotation = MKPointAnnotation() // используем что бы описать точку на карте
+            annotation.title = self.place.name
+            annotation.subtitle = self.place.type
+            
+            guard let placemarckLocation = placemark?.location else { return }
+            
+            annotation.coordinate = placemarckLocation.coordinate
+            
+            self.mapView.showAnnotations([annotation], animated: true)
+            self.mapView.selectAnnotation(annotation, animated: true) // что бы выделить созданую аннотацию у mapView
+        }
+        
+    }
+    
     
 }
--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -71,6 +71,17 @@ class NewPlaceViewController: UITableViewController {
         }
     }
     
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != "showMap" {
+            return
+        }
+        
+        let mapVC = segue.destination as! MapViewController
+        mapVC.place = currentPlace
+    }
+    
+    
     func savePlace(){
        // substitute the default picture or custom
         var image: UIImage?
